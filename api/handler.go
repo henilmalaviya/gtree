02c8c188b13e3339fadf121e1bc5dc5bca88c2c7
Henil Malaviya <henilm.dev@gmail.com>
2025-03-21 22:40:55 +0530

    feat: basic response proxy from github api

--- api/handler.go
+++ api/handler.go
@@ -1,12 +1,76 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"strings"
 )
 
+type ApiResponse struct {
+	SHA  string `json:"sha"`
+	Url  string `json:"url"`
+	Tree []struct {
+		Path string `json:"path"`
+		Mode string `json:"mode"`
+		Type string `json:"type"`
+		Size int    `json:"size"`
+		Sha  string `json:"sha"`
+		Url  string `json:"url"`
+	} `json:"tree"`
+}
+
+func fetchTree(owner, repo string) (ApiResponse, error) {
+	URL := fmt.Sprintf("https://api.github.com/repos/%s/%s/git/trees/main?recursive=true", owner, repo)
+
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		return ApiResponse{}, fmt.Errorf("GITHUB_TOKEN not set")
+	}
+
+	// prepare
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return ApiResponse{}, fmt.Errorf("error creating request: %w", err)
+	}
+
+	// set token
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+
+	// make request
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return ApiResponse{}, fmt.Errorf("error making request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// handle status code
+	if resp.StatusCode != http.StatusOK {
+		return ApiResponse{}, fmt.Errorf("request failed with status: %d", resp.StatusCode)
+	}
+
+	// read body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ApiResponse{}, fmt.Errorf("error reading response: %w", err)
+	}
+
+	// parse json
+	var data ApiResponse
+	if err := json.Unmarshal(body, &data); err != nil {
+		return ApiResponse{}, fmt.Errorf("error parsing JSON: %w", err)
+	}
+
+	return data, nil
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+
 	// removing first "/"
 	path := strings.TrimPrefix(r.URL.Path, "/")
 
@@ -20,7 +84,16 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		owner := parts[0]
 		repo := parts[1]
 
-		fmt.Fprintf(w, "Owner: %s, Repo: %s", owner, repo)
+		fetchedTree, err := fetchTree(owner, repo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// send response
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(fetchedTree)
+
 	} else {
 		http.Error(w, "Not Found", http.StatusNotFound)
 	}
