af144827660f43f9d3e411c0dbac4d4aaa25ddb1
Henil Malaviya <henilm.dev@gmail.com>
2025-03-21 22:15:56 +0530

    basic handling

--- api/handler.go
+++ api/handler.go
@@ -3,8 +3,25 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	// removing first "/"
+	path := strings.TrimPrefix(r.URL.Path, "/")
+
+	// split by "/"
+	parts := strings.Split(path, "/")
+
+	// pattern check (owner/repo)
+	if len(parts) == 2 && parts[0] != "" && parts[1] != "" {
+
+		// extract
+		owner := parts[0]
+		repo := parts[1]
+
+		fmt.Fprintf(w, "Owner: %s, Repo: %s", owner, repo)
+	} else {
+		http.Error(w, "Not Found", http.StatusNotFound)
+	}
 }
